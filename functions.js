113f128754f559188c64bb7dca473a5ad3f645a0
Eugen14102002 <95775034+Eugen14102002@users.noreply.github.com>
2022-10-10 20:31:16 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,11 +1,61 @@
-$(document).on('mousemove', function (e) {
-    basket.css('left', e.pageX);
+
+
+function elementsOverlap(el1, el2) {
+    const domRect1 = el1.getBoundingClientRect();
+    const domRect2 = el2.getBoundingClientRect();
+
+    return !(
+        domRect1.top > domRect2.bottom ||
+        domRect1.right < domRect2.left ||
+        domRect1.bottom < domRect2.top ||
+        domRect1.left > domRect2.right
+    );
+}
+
+const el1 = document.getElementById('basket');
+const el2 = document.getElementById('btnleft');
+const el3 = document.getElementById('btnright');
+
+console.log(elementsOverlap(el1, el2)); // 👉️ true
+
+// $('#left').click(() => {
+//     $('#right').removeAttr("disabled");
+//     $('#left').removeAttr("disabled");
+//     if (elementsOverlap(el1, el2)) {
+//         $('#left').attr("disabled", "true");
+//     }
+//     $('#basket').css({
+//         'marginLeft': "-=70px" //moves left
+//     });
+// });
+
+// $('#right').click(() => {
+//     $('#right').removeAttr("disabled");
+//     $('#left').removeAttr("disabled");
+//     if (elementsOverlap(el1, el3)) {
+//         $('#right').attr("disabled", "true");
+//     }
+//     $('#basket').css({
+//         'marginLeft': "+=70px" //moves left
+//     });
+// });
+
+$(document).ready(function () {
+    if (!window.matchMedia("(max-width: 1280px)").matches) {
+        $(document).on('mousemove', function (e) {
+            basket.css('left', e.pageX);
+        });
+    } else {
+
+    }
 });
+
 
 function egg_down(egg) {
     egg_current_position = parseInt(egg.css('top'));
     egg.css('top', egg_current_position + speed);
 }
+
 
 function check_egg_hits_floor(egg) {
     if (collision(egg, floor)) {
@@ -64,4 +114,81 @@ function stop_the_game() {
 
 restart.click(function () {
     location.reload();
+});
+
+
+let Rightinterval, Righttimeout, Leftimeout, Lefttimeout;
+
+function touchStartRight() {
+    rightfunc();
+    Rightinterval = setTimeout(function () {
+        Rightinterval = setInterval(function () {
+            rightfunc();
+        }, 10);
+    }, 10);
+}
+
+function touchEndRight() {
+    clearTimeout(Rightinterval);
+    clearInterval(Rightinterval);
+}
+
+function rightfunc() {
+    $('#left').css({
+        'display': "block" //moves left
+    });
+    $('#right').css({
+        'display': "block" //moves left
+    });
+    if (elementsOverlap(el1, el3)) {
+        clearTimeout(Rightinterval);
+        clearInterval(Rightinterval);
+        $('#right').css({
+            'display': "none" //moves left
+        });
+    }
+    $('#basket').css({
+        'marginLeft': "+=5px" //moves right
+    });
+}
+
+
+function touchStartLeft() {
+    leftfunc();
+    Rightinterval = setTimeout(function () {
+        Rightinterval = setInterval(function () {
+            leftfunc();
+        }, 10);
+    }, 10);
+}
+
+function touchEndLeft() {
+    clearTimeout(Rightinterval);
+    clearInterval(Rightinterval);
+}
+
+function leftfunc() {
+    $('#left').css({
+        'display': "block" //moves left
+    });
+    $('#right').css({
+        'display': "block" //moves left
+    });
+    if (elementsOverlap(el1, el2)) {
+        clearTimeout(Rightinterval);
+        clearInterval(Rightinterval);
+        $('#left').css({
+            'display': "none" //moves left
+        });
+    }
+    $('#basket').css({
+        'marginLeft': "-=5px" //moves left
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#right").addEventListener("touchstart", touchStartRight);
+    document.querySelector("#right").addEventListener("touchend", touchEndRight);
+    document.querySelector("#left").addEventListener("touchstart", touchStartLeft);
+    document.querySelector("#left").addEventListener("touchend", touchEndLeft);
 });
